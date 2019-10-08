de1fb49dedf66dfc74b119fb1b1c7f30f4c7b770
tamnguyencodev <tam.nguyen@code.vn>
2019-10-08 08:51:15 +0700

    preapare to deploy

---
+++ build/precache-manifest.6829b93fa4a36e3e54e01efa47ee3964.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e89a9594624e01f4603a62ef58760e9b",
+    "url": "/m-blog/index.html"
+  },
+  {
+    "revision": "4ec9f1749a69c07074db",
+    "url": "/m-blog/static/css/2.653462cc.chunk.css"
+  },
+  {
+    "revision": "da5c903e5407cbe9be5e",
+    "url": "/m-blog/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "4ec9f1749a69c07074db",
+    "url": "/m-blog/static/js/2.e59e8ea4.chunk.js"
+  },
+  {
+    "revision": "da5c903e5407cbe9be5e",
+    "url": "/m-blog/static/js/main.7cacca82.chunk.js"
+  },
+  {
+    "revision": "b7c9567ce8d3f643b786",
+    "url": "/m-blog/static/js/runtime-main.1ec5bf83.js"
+  },
+  {
+    "revision": "6292520e4254da645a19b38fe2c7c4fc",
+    "url": "/m-blog/static/media/avt.6292520e.jpg"
+  },
+  {
+    "revision": "53ecac61529a3eb4607ecffb28018287",
+    "url": "/m-blog/static/media/br-img.53ecac61.jpg"
+  },
+  {
+    "revision": "422e766543a3850de8db1f049775a1e9",
+    "url": "/m-blog/static/media/ic_facebook.422e7665.svg"
+  },
+  {
+    "revision": "89dcd6955d20dc3183df9d8ba408312c",
+    "url": "/m-blog/static/media/ic_linkedin.89dcd695.svg"
+  }
+]);
