f97b6e08045a9a9f2baa5b3835ae27fb8c5e6072
Albert <ndttam.dev@gmail.com>
2020-01-11 11:46:05 +0700

    edited package json

---
+++ build/precache-manifest.05496889fdbe038680a77c4e29016412.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7159c6af15fc288ea7abfe18806f1b97",
+    "url": "/my-blog/index.html"
+  },
+  {
+    "revision": "932e9bc25aa2e3e1b016",
+    "url": "/my-blog/static/css/2.53de959a.chunk.css"
+  },
+  {
+    "revision": "defc043acc5d5dec1a05",
+    "url": "/my-blog/static/css/main.890b028d.chunk.css"
+  },
+  {
+    "revision": "932e9bc25aa2e3e1b016",
+    "url": "/my-blog/static/js/2.ce3eae7a.chunk.js"
+  },
+  {
+    "revision": "b16c193132695216b3273f2acb3f96c7",
+    "url": "/my-blog/static/js/2.ce3eae7a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "defc043acc5d5dec1a05",
+    "url": "/my-blog/static/js/main.931369b9.chunk.js"
+  },
+  {
+    "revision": "31100115f8abe33dc967",
+    "url": "/my-blog/static/js/runtime-main.76c09252.js"
+  },
+  {
+    "revision": "6292520e4254da645a19b38fe2c7c4fc",
+    "url": "/my-blog/static/media/avt.6292520e.jpg"
+  },
+  {
+    "revision": "53ecac61529a3eb4607ecffb28018287",
+    "url": "/my-blog/static/media/br-img.53ecac61.jpg"
+  },
+  {
+    "revision": "959e205216b29c9093e34085d86f51d2",
+    "url": "/my-blog/static/media/ic_fb.959e2052.svg"
+  },
+  {
+    "revision": "9a988ea5ff1608a768fdbe46cd120d7f",
+    "url": "/my-blog/static/media/ic_github.9a988ea5.svg"
+  },
+  {
+    "revision": "da2d90df731c220969e420c9b133ea88",
+    "url": "/my-blog/static/media/ic_instagram.da2d90df.svg"
+  },
+  {
+    "revision": "9c9759b9f922bd3f106d9912deba9aa1",
+    "url": "/my-blog/static/media/ic_linkedin.9c9759b9.svg"
+  },
+  {
+    "revision": "d49f012416aad5a2f6e40d9c34735851",
+    "url": "/my-blog/static/media/shop-logo.d49f0124.png"
+  },
+  {
+    "revision": "44049cbca60035dbc92634d388236a6a",
+    "url": "/my-blog/static/media/signature.44049cbc.png"
+  }
+]);
