c48432db08851fce91407b3b1308252666a4ac06
Albert <ndttam.dev@gmail.com>
2020-01-13 21:19:24 +0700

    commit prepare to implement webpack

--- src/utils/paths/index.tsx
+++ src/utils/paths/index.tsx
@@ -2,8 +2,6 @@ export const PATH_ROOT = "/";
 export const PATH_BLOG_PAGE = "/blog-page";
 export const PATH_SHOP_PAGE = "/shop-page";
 
-
-
 // Socials
 export const LINK_FACEBOOK = "https://www.facebook.com/tam.pi.33";
 export const LINK_LINKEDIN = "https://www.linkedin.com/in/tam-nguyen-6a1576183/";
