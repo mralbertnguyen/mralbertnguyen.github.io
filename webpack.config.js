ad48b434fd024926d6fb801b7bddcacc0904f584
Albert <ndttam.dev@gmail.com>
2020-01-16 22:20:22 +0700

    peding

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,50 @@
+const path = require('path');
+const webpack = require('webpack');
 const HtmlWebPackPlugin = require("html-webpack-plugin");
-module.export={
-    module: {
-        rules:[
-            {
-                test: /\.(js|jsx|tsx|ts)$/,
-                exclude: /node_modules/,
-                use: {
-                  loader: "babel-loader"
-                }
-            },
-            {
-                test: /\.html$/,
-                use: [
-                  {
-                    loader: "html-loader"
-                  }
-                ]
-              }
+module.export = {
+  mode: 'development',
+  entry: {
+    app: ['./src/index.tsx', 'webpack-hot-middleware/client'],
+    vendor: ['react', 'react-dom']
+  },
+  output: {
+    filename: 'js/[name].bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['.js', '.jsx', '.json', '.ts', '.tsx']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "ts-loader"
+        }
+      },
+      { enforce: "pre", test: /\.js$/, loader: "source-map-loader" },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader"
+          }
         ]
-    },
-    plugins: [
-        new HtmlWebPackPlugin({
-          template: "./build/index.html",
-          filename: "./index.html"
-        })
-      ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({ template: path.resolve(__dirname, 'public', 'app', 'index.html') }),
+    new webpack.HotModuleReplacementPlugin()
+  ]
 
 }
