a628e3fb2b71964cfcef2a174709f8caa3608100
Albert Nguyen <ndttam.dev@gmail.com>
2020-01-20 20:42:41 +0700

    find idea

--- src/utils/paths/index.tsx
+++ src/utils/paths/index.tsx
@@ -1,11 +1,8 @@
 export const PATH_ROOT ="/";
 export const PATH_BLOG_PAGE = "/blog-page";
 export const PATH_SHOP_PAGE = "/shop-page";
-<<<<<<< HEAD
-=======
 export const PATH_DATE_PAGE ="/date-page";
 
->>>>>>> e30753c8ff23a71d4b42f6f7a2d543a4eda51795
 
 // Socials
 export const LINK_FACEBOOK = "https://www.facebook.com/tam.pi.33";
