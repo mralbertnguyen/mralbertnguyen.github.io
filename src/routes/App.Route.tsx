b2c978ace885303b39db9c97d5d4195ea904d220
albertnguyen <ndttam.dev@gmail.com>
2020-08-03 10:20:36 +0700

    refactoring

--- src/routes/App.Route.tsx
+++ src/routes/App.Route.tsx
@@ -1,33 +1,31 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import { PATH_ROOT, PATH_BLOG_PAGE, PATH_SHOP_PAGE, PATH_DATE_PAGE } from "../utils/paths"
-import { NotFound } from "../containers/Result"
+import { NotFound } from "../containers/NotFound"
 import AppLayout from '../layouts'
 
-import BlogPage from "../containers/BlogPage"
-import ShopPage from "../containers/ShopPage"
-import DatePage from "../containers/DatePage"
+// import DatePage from "../containers/DatePage"
 const DataRoute = [
     {
         key : "root",
         path: `${PATH_ROOT}`,
         component:AppLayout
     },
-    { 
-        key : "blog-page",
-        path: `${PATH_BLOG_PAGE}`,
-        component: BlogPage,
-    },
-    { 
-        key : "shop-page",
-        path: `${PATH_SHOP_PAGE}`,
-        component: ShopPage,
-    },
-    { 
-        key : "date-page",
-        path: `${PATH_DATE_PAGE}`,
-        component: DatePage,
-    },
+    // { 
+    //     key : "blog-page",
+    //     path: `${PATH_BLOG_PAGE}`,
+    //     component: BlogPage,
+    // },
+    // { 
+    //     key : "shop-page",
+    //     path: `${PATH_SHOP_PAGE}`,
+    //     component: ShopPage,
+    // },
+    // { 
+    //     key : "date-page",
+    //     path: `${PATH_DATE_PAGE}`,
+    //     component: DatePage,
+    // },
 ]
 
 const AppRoute = () => { 
